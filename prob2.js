3e9360effe8b0a36f199bade16dac7c916678ce5
takashi <takashi.sh@proton.me>
2023-05-10 19:19:16 -0400

    problem 2 array.push

--- prob2.js
+++ prob2.js
@@ -1,15 +1,19 @@
 function pushTest(n) {
     const arr = [];
-  
-    for (let i = 0; i < n; i++) {
+
+  for (let i = 0; i < n; i++) {
+    const startTime = Date.now()
       arr.push(i);
+    const endTime = Date.now()
+    const elapsedTime = endTime - startTime;
+    console.log(`Time Elapsed: ${elapsedTime}`)
+
     }
-  
+
     return arr;
   }
 
-/* let n = 100000;
+let n = 100000;
 console.time(`pushTest(${n}): `);
 pushTest(n);
 console.timeEnd(`pushTest(${n}): `);
-*/
