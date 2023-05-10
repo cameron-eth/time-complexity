e985750e57e53f4b3f97b387f0da39cdb6437ce1
Cameron Norfleet <cameron@Camerons-MBP.lan>
2023-05-10 16:48:35 -0700

    re repo

--- prob3.js
+++ prob3.js
@@ -1,9 +1,23 @@
 function popTest(arr) {
-    const n = arr.length;
-  
-    for (let i = 0 ; i < n ; i++) {
-      arr.pop();
-    }
+  const n = arr.length;
       
-    return arr;
+  for (let i = 0 ; i < n ; i++) {
+  console.time('pop')
+  arr.pop();
+  console.timeEnd('pop')
+  }
+        
+  return arr;
+  }
+  
+  let arrayPopulator = (num) => {
+      let res = []
+      for(let i = 0; i < num; i++){
+          res.push(i)
+      }
+      return res
 }
+console.time('finalAnswer');
+popTest(arrayPopulator(125222))
+console.timeEnd('finalAnswer');
+  
