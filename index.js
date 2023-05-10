b34f75f96f3062b636e4c63e0b15d46ade39e4d3
Cameron Norfleet <cameron@Camerons-MBP.lan>
2023-05-10 15:49:37 -0700

    prob1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function randomNumberArr(max) {
+  const res = new Array();
 
+  for (let i = 0; i < max; i++) {
+    const randomNumber = Math.floor(Math.random() * Math.floor(max));
+
+    res.push(randomNumber);
+  }
+
+  return res;
+}
