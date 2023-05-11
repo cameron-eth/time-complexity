6316b5088f6ba349a026390fcb544b1e05c0b579
takashi <takashi.sh@proton.me>
2023-05-10 20:27:20 -0400

    unshift

--- prob4.js
+++ prob4.js
@@ -1,9 +1,17 @@
 function unshiftTest(n) {
     const arr = [];
-  
-    for (let i = 0; i < n; i++) {
+
+  for (let i = 0; i < n; i++) {
+      console.time('unshift')
       arr.unshift(i);
+      console.timeEnd('unshift')
     }
-    
+
     return arr;
 }
+
+console.time("finalAnswer");
+unshiftTest(1234567)
+// popTest(arrayPopulator(125222));
+console.timeEnd("finalAnswer");
+//total time: 4:03.306
